710be33abdc26003ef4f3377344c9625ecc1362f
JosueTC94 <alu0100763492@ull.edu.es>
2016-11-24 20:06:00 +0000

    Actualizando versión

--- template/app.js
+++ template/app.js
@@ -9,99 +9,40 @@ var datos_config = JSON.parse(JSON.stringify(config));
 var logout = require('express-passport-logout');
 var expressLayouts = require('express-ejs-layouts');
 
-var Dropbox = require('dropbox');
-var dbx = new Dropbox({accessToken: datos_config.token_dropbox});
 var bcrypt = require("bcrypt-nodejs");
-var users;
-var datos;
-var nombre_bd;
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('P9.db');
+var funciones_db = require(path.join(basePath,'public','js','queries.js'));
+
+// Database initialization
+db.serialize(function(){
+  db.run("CREATE TABLE IF NOT EXISTS USUARIOS ( userID int, username varchar(255), password varchar(255),displayName varchar(255) );", function(err)
+  {
+    if(err)
+    {
+      console.log("ERROR CREANDO TABLA:"+err);
+      throw err;
+    }
+    console.log("SQL Table 'USUARIOS' initialized");
+  });
+});
 
 passport.use(new LocalStrategy(
   function(username, password, cb) {
-      dbx.sharingGetSharedLinkFile({ url: datos_config.link_bd})
-          .then(function(data)
-          {
-              // console.log("BODY:"+JSON.stringify(data));
-              nombre_bd = data.name;
-              console.log("NAME:"+nombre_bd);
-              // console.log("Users:"+JSON.stringify(data.fileBinary));
-              datos = JSON.parse(data.fileBinary);
-              console.log("Datos:"+datos);
+    console.log("Estrategia de local");
+    console.log("User:"+username);
+    console.log("Password:"+password);
 
-              users = datos.users;
-
-              console.log("USERS:"+users);
-
-              var queries_bd = require(path.join(basePath,'public','js','queries.js'));
-
-              queries_bd.findByUsername(datos.users,username, function(err, usuario)
-              {
-                console.log("USUARIO:"+usuario.username+",passs:"+usuario.password);
-                console.log("password:"+password);
-                if(err)
-                {
-                  console.log("1");
-                  throw err;
-                  return cb(err);
-                }
-                if(!usuario){
-                  console.log("El usuario no se encuentra en la base de datos");
-                  return cb(null,false);
-                }
-                console.log("Usuario encontrado:"+JSON.stringify(usuario));
-
-                try {
-                  if(bcrypt.compareSync(password, usuario.password) == false)
-                  {
-                    console.log("Password incorrecto");
-                    return cb(null,false);
-                  }
-                  else
-                  {
-                      console.log("TODO OK");
-                      return cb(null,usuario);
-                  }
-                } catch (e) {
-                  console.log("error:"+e);
-                  if(password == usuario.password)
-                  {
-                    //No está encriptada
-                      console.log("EAAA");
-                      var new_password = password;
-                      var hash = bcrypt.hashSync(new_password);
-
-                      for(var i=0,len = users.length; i < len; i++)
-                      {
-                         console.log("i:"+i, "Name:"+users[i].username);
-                         if(users[i].username == username)
-                         {
-                           console.log("ENCONTRADO");
-                           users[i].password = hash;
-                           break;
-                         }
-                      }
-
-                      dbx.filesUpload({path: '/'+nombre_bd, contents: JSON.stringify(datos), mode: "overwrite"})
-                        .then(function(response)
-                        {
-                          console.log("Response:"+JSON.stringify(response));
-                        })
-                        .catch(function(err)
-                        {
-                          console.error(err);
-                        });
-
-                        return cb(null,usuario);
-                  }
-                  return cb(null,false);
-                }
-                return cb(null,usuario);
-              });
-          })
-          .catch(function(err)
-          {
-              console.error(err);
-          });
+    //SELECTTTTTT
+    funciones_db.findByUsername(db,username,password, (err,usuario) =>
+    {
+        if(err)
+        {
+          console.log("Err:"+err);
+          throw err;
+        }
+        return cb(null,usuario);
+    });
   }
 ));
 
@@ -152,7 +93,7 @@ app.get('/',
 app.get('/login',
   passport.authenticate('local', {failureRedirect: '/error'}),
   function(req,res) {
-	res.render('login', {user: req.user});
+	res.render('login', {user: req.user[0]});
 });
 
 app.get('/change_password', function(req,res)
@@ -160,33 +101,20 @@ app.get('/change_password', function(req,res)
     res.render('changing_password',{user: req.user});
 });
 
-app.get('/change_password/return', function(req,res)
+app.get('/change_password_return', function(req,res)
 {
-    var new_password = req.query.new_pass;
-    var hash = bcrypt.hashSync(new_password);
-    var new_password_encripted = bcrypt.compareSync(new_password, hash);
-    
-    console.log("hash:"+hash);
-    //ACTUALIZAMOS CONTENIDO DE USERS
-    for(var i=0,len = users.length; i < len; i++)
+  // console.log("PASSWORD:"+req.query.new_pass);
+  // console.log("ROWWWWW BABY:"+JSON.stringify(req.user[0]));
+  // ACTUALIZANDO
+  funciones_db.change_password(db,req.user[0].username,req.query.new_pass,(err) =>
+  {
+    if(err)
     {
-       if(users[i].username == req.user.username)
-       {
-         users[i].password = hash;
-         break;
-       }
+      console.log("ERROR:"+err);
+      throw err;
     }
-    //SUBIMOS FICHERO A DROPBOX
-    dbx.filesUpload({path: '/'+nombre_bd, contents: JSON.stringify(datos), mode: "overwrite"})
-	.then(function(response)
-	{
-		res.redirect('/');
-	})
-	.catch(function(err)
-	{
-		console.log(err);
-	});
-	return false;
+    res.render('login',{user: req.user[0]});
+  });
 });
 
 app.get('/inicio_gitbook', function(req,res)
