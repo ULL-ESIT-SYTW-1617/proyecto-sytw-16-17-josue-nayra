293efc4db345635a6a6ff11a8cff560a9bfbee50
Nayra Rguez <alu0100406122@ull.edu.es>
2016-11-30 18:26:05 +0000

    Añadiendo modelo para Sequelize.

---
+++ template/models/usuarios.js
@@ -0,0 +1,43 @@
+
+module.exports = function(sequelize, DataTypes) {
+    var User = sequelize.define(
+        'User', 
+        { 
+            username: {
+                type: DataTypes.STRING,
+                unique: true,
+                validate: { 
+                    notEmpty: {msg: "-> Falta username"},
+                    // hay que devolver un mensaje de error si el username ya existe
+                    isUnique: function (value, next) {
+                        var self = this;
+                        User.find({where: {username: value}})
+                        .then(function (user) {
+                                if (user && self.id !== user.id) {
+                                    return next('Username ya utilizado');
+                                }
+                                return next();
+                        })
+                        .catch(function (err) {
+                            return next(err);
+                        });
+                    }
+                }
+            },
+            password: {
+                type: DataTypes.STRING,
+                validate: { notEmpty: {msg: "-> Falta password"}},
+                // set: function (password) {
+                //     // var encripted = crypto.createHmac('sha1', key).update(password).digest('hex');
+                //     // Evita passwords vacíos
+                //     if (password === '') {
+                //         encripted = '';
+                //     }
+                //     this.setDataValue('password', encripted);
+                // }
+            }   
+        }
+    );
+
+    return User;
+}
